b9fada2f8402d1d57595b7a79a6d46bc439b9f61
Paul Shynin <pshynin@pentaho.com>
2017-11-14 17:27:28 -0500

    added Google Search demo-test

---
+++ src/test/java/puc/GoogleTest.java
@@ -0,0 +1,11 @@
+package puc;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by pshynin on 11/14/2017.
+ */
+@RunWith(Cucumber.class)
+public class GoogleTest {
+}
--- src/test/java/puc/PUC_FunctionalTest_Login.java
+++ src/test/java/puc/PUC_FunctionalTest_Login.java
@@ -1,35 +1,39 @@
 package puc;
 
-import com.codeborne.selenide.junit.ScreenShooter;;
+import com.codeborne.selenide.Configuration;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
 
-import static com.codeborne.selenide.Condition.disappears;
-import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.*;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.FIREFOX;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+;
 
 /**
  * Created by pshynin on 11/10/2017.
  */
 public class PUC_FunctionalTest_Login {
     private LoginPage page;
+    Configuration configuration = new Configuration();
 
-   // @Rule
-    public ScreenShooter screenShooter = ScreenShooter.failedTests();
+    // @Rule
+    // public ScreenShooter screenShooter = ScreenShooter.failedTests();
 
-    @BeforeClass
-    public static void init() {
-        timeout = 10000;
-        baseUrl = "http://localhost:8080/pentaho/PUC_Login_StepDefinitions";
-        browser = FIREFOX;
-        startMaximized = false;
-        open(baseUrl, LoginPage.class);
-        waitUntilPagesIsLoaded();
+    @Test
+    public void init() {
+
+        //baseUrl = "http://localhost:8080/pentaho";
+        //browser = FIREFOX;
+        //startMaximized = false;
+        page = open(baseUrl, LoginPage.class);
+        // waitUntilPagesIsLoaded();
     }
 
     @Test
@@ -43,7 +47,7 @@ public class PUC_FunctionalTest_Login {
         closeWebDriver();
     }
 
-    protected static void waitUntilPagesIsLoaded() {
+/*    protected static void waitUntilPagesIsLoaded() {
         $(byText("Loading")).waitUntil(disappears, 20000);
-    }
+    }*/
 }
